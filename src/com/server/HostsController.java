78196fe166abae481c9fda738aea98f30ee90f8f
krego <mateusz.olczynski@gmail.com>
2016-12-28 20:54:28 +0100

    Remove final keyword from Hosts Controller class definiton

--- src/com/server/HostsController.java
+++ src/com/server/HostsController.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 /**
  * Created by krego on 28.12.2016.
  */
-public final class HostsController {
-    static void startServers(ArrayList<Host> hosts, int portNumber){
+public class HostsController {
+    public static void startServers(ArrayList<Host> hosts, int portNumber){
         try {
             hosts.add(new Host(portNumber, hosts.size(), 1));
         } catch (IOException e) {
